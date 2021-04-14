b6355792cb9f2a39b80314a14a6d6c29b2b95674
ogunsoladebayo <ogunsoladebayo@gmail.com>
2021-04-14 13:39:42 +0100

    added url validation before generating shortenUrl

--- schema.js
+++ schema.js
@@ -1,5 +1,20 @@
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require('graphql');
 const generate = require('./controllers/generate');
+const validUrl = require('valid-url');
+
+// URL validation check
+const isValidUrl = (url) => {
+	var pattern = new RegExp(
+		'^(https?:\\/\\/)?' + // protocol
+		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+		'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+		'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+			'(\\#[-a-z\\d_]*)?$',
+		'i'
+	); // fragment locator
+	return !!pattern.test(url);
+};
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
@@ -9,8 +24,11 @@ const RootQuery = new GraphQLObjectType({
 			type: GraphQLString,
 			args: { url: { type: GraphQLString } },
 			resolve(parentValue, args) {
+				if (!isValidUrl(args.url)) {
+					return 'Not a valid URL';
+				}
 				return generate(args.url).then(
-					(res) => `https://biturl.tk/${res}`
+					(res) => `${process.env.DOMAIN_URL}/${res}`
 				);
 			}
 		}
